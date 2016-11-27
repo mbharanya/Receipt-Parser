90bc855b4a2a2ff1b19287359da8f685dc4138fe
Milan Bharanya <spam@trash.li>
2016-11-27 12:49:26 +0100

    Using config.properties for Migros CSV columns

--- src/main/java/ch/bharanya/receipt_parser/parser/migros/EMigrosOverviewCsvHeader.java
+++ src/main/java/ch/bharanya/receipt_parser/parser/migros/EMigrosOverviewCsvHeader.java
@@ -1,13 +1,15 @@
 package ch.bharanya.receipt_parser.parser.migros;
 
+import ch.bharanya.receipt_parser.config.Config;
+
 public enum EMigrosOverviewCsvHeader {
-	DATE("Datum"),
-	TIME("Zeit"),
-	LOCATION("Filiale"),
-	CHECKOUT_NUMBER("Kassennummer"),
-	TRANSACTION_NUMBER("Transaktionsnummer"),
-	TOTAL_PRICE("Einkaufsbetrag"),
-	CUMULUS_POINTS("Bonuspunkte");
+	DATE(Config.getInstance().getProperty("migros.parser.csv.overview.dateCol")),
+	TIME(Config.getInstance().getProperty("migros.parser.csv.overview.timeCol")),
+	LOCATION(Config.getInstance().getProperty("migros.parser.csv.overview.locationCol")),
+	CHECKOUT_NUMBER(Config.getInstance().getProperty("migros.parser.csv.overview.checkOutNumberCol")),
+	TRANSACTION_NUMBER(Config.getInstance().getProperty("migros.parser.csv.overview.transactionNumberCol")),
+	TOTAL_PRICE(Config.getInstance().getProperty("migros.parser.csv.overview.totalPriceCol")),
+	CUMULUS_POINTS(Config.getInstance().getProperty("migros.parser.csv.overview.cumulusPointsCol"));
 	
 	private String colName;
 
